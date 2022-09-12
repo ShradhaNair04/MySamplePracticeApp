41cd4db6be9d1fd52f1d761df5bd6e5f9188bffd
Shradha Nair <shradha.nair@deltatre.com>
2022-09-12 20:19:31 +0530

    Commit files for git

---
+++ API/Controllers/-- SQLitetest.sql
@@ -0,0 +1,10 @@
+-- SQLite
+INSERT INTO User (Id, UserName)
+VALUES (1,"Shree");
+
+INSERT INTO User (Id, UserName)
+VALUES (2,"Shradha");
+
+INSERT INTO User (Id, UserName)
+VALUES (3,"Jayshree");
+
